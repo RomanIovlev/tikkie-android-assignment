dd7bb1ef6f203de079f1aa89164cda1218e0b7b5
Roman Iovlev <romanyister@gmail.com>
2026-01-18 17:35:32 +0100

    Add failed test

--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/FailedTests.kt
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/FailedTests.kt
@@ -3,36 +3,22 @@ package com.abnamro.apps.referenceandroid
 import com.abnamro.apps.referenceandroid.screen.HelloWorldScreen
 import org.junit.Test
 
-class SmokeTests : TestSuite() {
+class FailedTests : TestSuite() {
     @Test
-    fun runAppSuccessfulTest() = run("Run App Successful Test") {
+    fun failingTest() = run("Test that failed because of wrong text") {
         HelloWorldScreen {
             screenStep ("Verify Hello World app screen") {
                 step("Title equals 'ReferenceAndroid'") {
                     pageTitle.hasTitle("ReferenceAndroid")
                 }
                 step("Main text is 'Hello World!'") {
-                    mainText.hasText("Hello World!")
+                    mainText.hasText("Another World")
                 }
                 step("Email button is visible") {
                     emailButton.isDisplayed()
                 }
                 step("Overflow menu button is visible") {
                     overflowMenuButton.isDisplayed()
-                }
-            }
-        }
-    }
-
-    @Test
-    fun contextMenuTest() = run("Context menu Test") {
-        HelloWorldScreen {
-            showOverflowMenu()
-            screenStep ("Verify context menu item is 'Settings'") {
-                settingItem {
-                    inRoot { isPlatformPopup() }
-                    isDisplayed()
-                    hasText("Settings")
                 }
             }
         }
