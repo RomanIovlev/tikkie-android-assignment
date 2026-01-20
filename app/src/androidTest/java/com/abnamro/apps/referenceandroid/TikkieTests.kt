a57f5af279bdf2148bc2228a9ade4925126b33f1
Roman Iovlev <romanyister@gmail.com>
2026-01-20 01:04:11 +0100

    merge tikkie tests

--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/TikkieTests.kt
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/TikkieTests.kt
@@ -4,10 +4,25 @@ import com.abnamro.apps.referenceandroid.screens.MainScreen
 import com.abnamro.apps.referenceandroid.screens.PaymentDetailsScreen
 import com.abnamro.apps.referenceandroid.screens.RequestPaymentStep1Screen
 import com.abnamro.apps.referenceandroid.screens.RequestPaymentStep2Screen
+import com.abnamro.apps.referenceandroid.testdata.dinnerDetails
 import com.abnamro.apps.referenceandroid.testdata.newPayment
 import org.junit.Test
 
-class RequestPaymentTests : TestSuite() {
+class TikkieTests : TestSuite() {
+    @Test
+    fun testDinnerPaymentDetails() = run("Test Dinner Payment Details") {
+        MainScreen {
+            screenStep("Tikkie app main screen open") {
+                addPaymentButton.isDisplayed()
+            }
+            selectPaymentByTitle(dinnerDetails.title)
+        }
+        PaymentDetailsScreen {
+            screenStep("Check all texts on Dinner details") {
+                verifyAllTexts(dinnerDetails)
+            }
+        }
+    }
     @Test
     fun testRequestPaymentFlow() = run("Test Request Payment Flow") {
         MainScreen {
@@ -19,8 +34,9 @@ class RequestPaymentTests : TestSuite() {
             }
         }
         RequestPaymentStep1Screen {
-            screenStep("Type '${newPayment.amount}'") {
-                amountInput.replaceText(newPayment.amount.toString())
+            val newPaymentAsString = newPayment.amount.toString().replace("\\.?0+$".toRegex(), "")
+            screenStep("Type '$newPaymentAsString'") {
+                amountInput.replaceText(newPaymentAsString)
             }
             screenStep("Click Next button") {
                 nextButton.click()
